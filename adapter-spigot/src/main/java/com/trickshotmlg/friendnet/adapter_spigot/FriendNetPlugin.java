fa145f417f5c4a0c36e7213d0c3e80b1fac3396b
TrickShotMLG02 <tim.schlachter@shinternet.ch>
2025-10-02 01:57:22 +0200

    refactored plugin sample structure

--- adapter-spigot/src/main/java/com/trickshotmlg/friendnet/adapter_spigot/FriendNetPlugin.java
+++ adapter-spigot/src/main/java/com/trickshotmlg/friendnet/adapter_spigot/FriendNetPlugin.java
@@ -12,36 +12,61 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class FriendNetPlugin extends JavaPlugin {
 
     private FriendService friendService;
+    private Platform platform;
 
     @Override
     public void onEnable() {
-        Platform platform = new SpigotPlatform(this);
-        this.friendService = new FriendServiceImpl();
-
-        // Example: listen to join/quit events and update online state
-        getServer().getPluginManager().registerEvents(new Listener() {
-            @EventHandler
-            public void onJoin(PlayerJoinEvent event) {
-                friendService.setOnline(event.getPlayer().getUniqueId(), true);
-
-                String joinLog = "Player: " + event.getPlayer().getDisplayName() + " joined!";
-                getLogger().info(joinLog);
-            }
-
-            @EventHandler
-            public void onQuit(PlayerQuitEvent event) {
-                friendService.setOnline(event.getPlayer().getUniqueId(), false);
-
-                String quitLog = "Player: " + event.getPlayer().getDisplayName() + " quited!";
-                getLogger().info(quitLog);
-            }
-        }, this);
-
+        initializePlatform();
+        initializeServices();
+        registerListeners();
         getLogger().info("FriendNet enabled!");
+    }
+
+    private void initializePlatform() {
+        this.platform = new SpigotPlatform(this);
+    }
+
+    private void initializeServices() {
+        this.friendService = new FriendServiceImpl();
+    }
+
+    private void registerListeners() {
+        getServer().getPluginManager().registerEvents(new PlayerStatusListener(friendService), this);
     }
 
     public FriendService getFriendService() {
         return friendService;
     }
-}
 
+    /**
+     * Listener class for player join/quit events.
+     */
+    private static class PlayerStatusListener implements Listener {
+
+        private final FriendService friendService;
+
+        public PlayerStatusListener(FriendService friendService) {
+            this.friendService = friendService;
+        }
+
+        @EventHandler
+        public void onPlayerJoin(PlayerJoinEvent event) {
+            SpigotPlayer spigotPlayer = new SpigotPlayer(event.getPlayer());
+
+            friendService.setOnline(spigotPlayer.getUniqueId(), true);
+            log(spigotPlayer.getName() + " joined!");
+        }
+
+        @EventHandler
+        public void onPlayerQuit(PlayerQuitEvent event) {
+            SpigotPlayer spigotPlayer = new SpigotPlayer(event.getPlayer());
+
+            friendService.setOnline(spigotPlayer.getUniqueId(), false);
+            log(spigotPlayer.getName() + " quit!");
+        }
+
+        private void log(String message) {
+            System.out.println("[FriendNet] " + message); // simple logging
+        }
+    }
+}
